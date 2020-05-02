6de0e7e8851932d5c401ad41c78f35318d9454e7
Kyle Downey <kyle.downey@gmail.com>
2020-05-02 09:35:35 -0400

    Add first test

---
+++ tests/tau/test_core.py
@@ -0,0 +1,19 @@
+from unittest.mock import Mock
+
+from tau.core import Network, Event
+
+
+def test_event_propagation():
+    network = Network()
+
+    a = Mock(spec=Event)
+    a.on_activate.return_value = True
+    b = Mock(spec=Event)
+    c = Mock(spec=Event)
+
+    network.connect(a, b)
+    network.connect(a, c)
+    network.activate(a)
+    a.on_activate.assert_called_once()
+    b.on_activate.assert_called_once()
+    c.on_activate.assert_called_once()
